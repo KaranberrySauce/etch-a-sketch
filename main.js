167b27b5b80ac377cd8e51f2c6e0825a617cf679
Karanvir Sidhu <karan.sidhu98@gmail.com>
2023-07-07 15:29:00 -0700

    Complete base etch a sketch

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 let color = "black";
-
+let click = false;
 document.addEventListener("DOMContentLoaded", function () {
   createBoard(16);
+
+  document.querySelector("body").addEventListener("click", function (e) {
+    if (e.target.tagName != "BUTTON") {
+      click = !click;
+      let draw = document.querySelector("#draw");
+      if (click) {
+        draw.innerHTML = "You can draw now";
+      } else {
+        draw.innerHTML = "You cannot draw now";
+      }
+    }
+  });
+
   let btn_popup = document.querySelector("#popup");
   btn_popup.addEventListener("click", function () {
     let size = getSize();
@@ -37,10 +50,12 @@ function getSize() {
 }
 
 function colorDiv() {
-  if (color == "random") {
-    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-  } else {
-    this.style.backgroundColor = "black";
+  if (click) {
+    if (color == "random") {
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+      this.style.backgroundColor = "black";
+    }
   }
 }
 
@@ -49,6 +64,6 @@ function setColor(colorChoice) {
 }
 
 function resetBoard() {
-    let divs = document.querySelectorAll("div");
-    divs.forEach(div => div.style.backgroundColor = "white");
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => (div.style.backgroundColor = "white"));
 }
