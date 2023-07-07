f033681db24c6a18ea3a4773dfc3a5a56564258a
Karanvir Sidhu <karan.sidhu98@gmail.com>
2023-07-07 15:19:54 -0700

    working with buttons and colours

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
+let color = "black";
+
 document.addEventListener("DOMContentLoaded", function () {
-  createBoard(64);
-  console.log("DOM loaded with JavaScript");
+  createBoard(16);
+  let btn_popup = document.querySelector("#popup");
+  btn_popup.addEventListener("click", function () {
+    let size = getSize();
+    createBoard(size);
+  });
 });
 
 function createBoard(size) {
@@ -12,7 +18,37 @@ function createBoard(size) {
 
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = "yellow";
+    div.addEventListener("mouseover", colorDiv);
     board.insertAdjacentElement("beforeend", div);
   }
+}
+
+function getSize() {
+  let input = prompt("Please input the size of the board: ");
+  let message = document.querySelector("#message");
+  if (input == "") {
+    message.innerHTML = "Plesase input a number";
+  } else if (input < 0 || input > 100) {
+    message.innerHTML = "Plesase input a number between 1 and 100";
+  } else {
+    message.innerHTML = "The size of the board is " + input;
+    return input;
+  }
+}
+
+function colorDiv() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "black";
+  }
+}
+
+function setColor(colorChoice) {
+  color = colorChoice;
+}
+
+function resetBoard() {
+    let divs = document.querySelectorAll("div");
+    divs.forEach(div => div.style.backgroundColor = "white");
 }
